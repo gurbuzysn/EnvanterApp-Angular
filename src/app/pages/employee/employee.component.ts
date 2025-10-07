25c148683229f53ba0f21ec6c8c80875fdc03374
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-10-07 07:27:16 +0300

    Font-Family : Poppins

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,16 +1,32 @@
 import { Component } from "@angular/core";
-import { InputText } from "primeng/inputtext";
+import { InputTextModule } from "primeng/inputtext";
 import { IconFieldModule } from "primeng/iconfield";
 import { InputIconModule } from "primeng/inputicon";
+import { CommonModule } from "@angular/common";
+
 
 @Component({
     selector: 'app-employee',
     standalone: true,
-    imports: [InputText, IconFieldModule, InputIconModule],
+    imports: [InputTextModule, IconFieldModule, InputIconModule, CommonModule],
     templateUrl: './employee.component.html'
 
 })
 
 export class EmployeeComponent{
+searchText: string = '';
 
+
+     employees = [
+    { name: 'Yasin Gürbüz', email: 'yasin@example.com', phone: '0500 111 22 33', department: 'IT' },
+    { name: 'Ayşe Demir', email: 'ayse@example.com', phone: '0500 444 55 66', department: 'Muhasebe' },
+    { name: 'Ali Kaya', email: 'ali@example.com', phone: '0500 999 88 77', department: 'İnsan Kaynakları' },
+  ];
+
+     filteredEmployees() {
+    if (!this.searchText) return this.employees;
+    return this.employees.filter(e =>
+      e.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 }
